a3d62c64340cc5a3df678bdb07a7577d5399d780
Virhuez Michael <virhuezmichael@gmail.com>
2025-05-11 07:35:56 -0300

    week02 - classes exercises

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2010;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data Scientist";
+        job2._company = "Microsoft";
+        job2._startYear = 2015;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Michael Virhuez";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
     }
 }
+
